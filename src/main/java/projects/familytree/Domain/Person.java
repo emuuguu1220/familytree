6554533aa112b3100c1a2af0047e163602d2b68d
Munkhzorig <emuuguu1220@gmail.com>
2020-06-13 18:11:53 -0500

    ad

--- src/main/java/projects/familytree/Domain/Person.java
+++ src/main/java/projects/familytree/Domain/Person.java
@@ -1,7 +1,12 @@
 package projects.familytree.Domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -9,36 +14,53 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public @Data class Person {
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private Date DOB;
-    private String firstName;
-    private String lastName;
-    private String familyName;
-    @JsonIgnore
+    @Getter @Setter private long id;
+    @Getter @Setter private Date DOB;
+    @Getter @Setter private String firstName;
+    @Getter @Setter private String lastName;
+    @Getter @Setter private String familyName;
     @OneToMany(mappedBy = "father")
-    private List<Person> childrenIfFather;
-    @JsonIgnore
+    @Getter @Setter private List<Person> childrenIfFather;
     @OneToMany(mappedBy = "mother")
-    private List<Person> childrenIfMother;
+    @Getter @Setter private List<Person> childrenIfMother;
 
     @ManyToMany
-    private List<Person> spouses;
+    @Getter @Setter private List<Person> spouses;
 
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Person father;
-    @ManyToOne
-    private Person mother;
-    private Gender gender;
-    private String phoneNumber;
-    private String other;
-    private String maritalStatus;
-    private Date DOD;
-    @Email
-    private String email;
-    public List<Person> getChildren(){
-        return gender == Gender.Эмэгтэй ? childrenIfMother : childrenIfFather;
+    @JsonIgnore
+    public Person getFather() {
+        return father;
     }
+    public void setFather(Person father) {
+        this.father = father;
+    }
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Person mother;
+    @JsonIgnore
+    public Person getMother() {
+        return mother;
+    }
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    @Getter @Setter private Gender gender;
+    @Getter @Setter private String phoneNumber;
+    @Getter @Setter private String other;
+    @Getter @Setter private String maritalStatus;
+    @Getter @Setter private Date DOD;
+    @Email
+    @Getter @Setter private String email;
+//    @JsonGetter("childrenIfMother")
+//    public List<Person> getChildren(){
+//        return gender == Gender.Эмэгтэй ? childrenIfMother : childrenIfFather;
+//    }
 }
