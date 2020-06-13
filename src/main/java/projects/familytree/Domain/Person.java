ee716921999bca6d010d2d2fc5462926881d6d02
Munkhzorig <emuuguu1220@gmail.com>
2020-06-12 19:36:11 -0500

    model

--- src/main/java/projects/familytree/Domain/Person.java
+++ src/main/java/projects/familytree/Domain/Person.java
@@ -16,51 +16,23 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter
     private int id;
-    @Getter
-    @Setter
-    private Date DOB;
-    @Getter
-    @Setter
-    private String firstName;
-    @Getter
-    @Setter
-    private String lastName;
-    @Getter
-    @Setter
-    private String familyName;
-    @Getter
-    @Setter
-    @OneToMany
+    @Getter @Setter private Date DOB;
+    @Getter @Setter private String firstName;
+    @Getter @Setter private String lastName;
+    @Getter @Setter private String familyName;
+    @Getter @Setter @OneToMany
     private List<Person> children;
-    @Getter
-    @Setter
-    @ManyToMany
+    @Getter @Setter @ManyToMany
     private List<Person> spouses;
-    @Getter
-    @Setter
-    @ManyToOne
+    @Getter @Setter @ManyToOne
     private Person father;
-    @Getter
-    @Setter
-    @ManyToOne
+    @Getter @Setter @ManyToOne
     private Person mother;
-    @Getter
-    @Setter
-    private Gender gender;
-    @Getter
-    @Setter
-    private String phoneNumber;
-    @Getter
-    @Setter
-    private String other;
-    @Getter
-    @Setter
-    private String maritalStatus;
-    @Getter
-    @Setter
-    private Date DOD;
-    @Getter
-    @Setter
-    @Email
+    @Getter @Setter private Gender gender;
+    @Getter @Setter private String phoneNumber;
+    @Getter @Setter private String other;
+    @Getter @Setter private String maritalStatus;
+    @Getter @Setter private Date DOD;
+    @Getter @Setter @Email
     private String email;
 }
